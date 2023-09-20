0749fb88c084b262f3d8a8ccf5d3b2d4736528fd
Jason Z <zhoujason32@gmail.com>
2023-09-20 08:19:54 -0500

    Created three screens

---
+++ app/src/main/java/com/example/categorydetailexampleapp/Food.java
@@ -0,0 +1,53 @@
+package com.example.categorydetailexampleapp;
+
+public class Food {
+    private String name;
+
+    private double price;
+
+    private String desc;
+
+    private int imageResourceID;
+
+    // Code -> Generate -> Constructor
+    public Food(String name, double price, String desc, int imageResourceID) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+        this.imageResourceID = imageResourceID;
+    }
+
+    // get/set methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+}
