1d53119ec9c998e718c207d822672012660584ce
Jason Z <zhoujason32@gmail.com>
2023-09-21 08:18:49 -0500

    Created Food[] arrays containing breakfast, lunch, and dinner options. Created new ListView in activitymain.xml that gives the three types of food.

--- app/src/main/java/com/example/categorydetailexampleapp/Food.java
+++ app/src/main/java/com/example/categorydetailexampleapp/Food.java
@@ -17,6 +17,28 @@ public class Food {
         this.imageResourceID = imageResourceID;
     }
 
+
+    public static final Food[] myBreakfast = {
+            new Food("Pizza", 13.99, "6 Slices, Cheese, Fresh Basil", R.drawable.pizza),
+            new Food("Toast", 7.50, "2 Pieces, Golden Brown, Honey-Coated", R.drawable.toast),
+            new Food("Bagel", 4.50, "1 Bagel, Cream Cheese", R.drawable.bagel),
+            new Food("Pie", 5.50, "1 Slice, Apple Filling", R.drawable.bagel)
+
+    };
+
+    public static final Food[] myLunch = {
+            new Food("Pizza", 13.99, "6 Slices, Cheese, Fresh Basil", R.drawable.pizza),
+            new Food("Grilled Cheese", 7.99, "2 Pieces, Cheese, Toasted Bread", R.drawable.grilledcheese),
+            new Food("Noodles", 8.50, "1 Bowl, Spicy", R.drawable.noodles)
+    };
+
+    public static final Food[] myDinner = {
+            new Food("Soup", 13.99, "1 Bowl, Tomato, Croutons", R.drawable.soup),
+            new Food("Pasta", 9.99, "1 Bowl, Cheese, Alfredo Sauce", R.drawable.pasta),
+            new Food("Chicken Tenders", 8.50, "7 Pieces, Barbecue Sauce", R.drawable.chicken)
+    };
+
+
     // get/set methods
 
     public String getName() {
