7515889e894455d383a407329c7cadb24ed72711
Jason Z <zhoujason32@gmail.com>
2023-09-25 08:20:18 -0500

    Linked pages together

--- app/src/main/java/com/example/categorydetailexampleapp/DetailActivity.java
+++ app/src/main/java/com/example/categorydetailexampleapp/DetailActivity.java
@@ -2,7 +2,12 @@ package com.example.categorydetailexampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        try {
+            // This gets the particular Detailed object we are displaying
+            // In this case, it is a Food object.
+            Food myFood = intent.getParcelableExtra("Chosen Food");
+            // This page will display the specific Detailed data for what your app is showing.
+            // Get references to the xml views for name, price, desc, and photo
+            TextView nameTV = findViewById(R.id.nameTextView);
+            TextView priceTV = findViewById(R.id.priceTextView);
+            TextView descTV = findViewById(R.id.descriptionTextView);
+            ImageView image = findViewById(R.id.imageView);
+            // set values on the screen based on the object that was passed to this Detail activity
+            nameTV.setText(myFood.getName());
+            priceTV.setText(String.valueOf(myFood.getPrice()));
+            descTV.setText(myFood.getDesc());
+            image.setImageResource(myFood.getImageResourceID());
+
+            if (myFood.getImageResourceID() == 0) {
+                // set a default pic or decide what to do in this case.
+            }
+            else {
+                image.setImageResource(myFood.getImageResourceID());
+            }
+        }
+        catch (Exception e) {
+            Log.i("Denna", "Food app not working!");
+        }
     }
+
 }
